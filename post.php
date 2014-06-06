fb61873a406bde985c7dc3090422058e2e759fb9
soutar <john@soutar.me>
2014-06-07 00:20:54 +0100

    Initial commit

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+
+$data = $_POST['piggy'];
+echo "<img src=\"{$data}\">";
+
+?>
---
+++ scribble.0.3.js
@@ -0,0 +1,118 @@
+function ScribbleInput(inputElem) {
+  this.input = {
+    elem: inputElem,
+    classList: $(inputElem).attr('class'), 
+    size: {
+      x: $(inputElem).attr('width'),
+      y: $(inputElem).attr('height')
+    },
+    src: $(inputElem).attr('src')
+  };
+  this.canvas = {};
+  this.lastXY = {x: null, y: null};
+}
+
+ScribbleInput.prototype.init = function () {
+  var imageObj,
+      self = this;
+
+  /* Create out canvas and copy classes from our input */
+  self.canvas.elem = $('<canvas>')[0];
+  $(self.canvas.elem)
+    .attr('class', self.input.classList)
+    .attr('width', self.input.size.x)
+    .attr('height', self.input.size.y);
+
+  /* Throw our canvas element into the DOM */
+  $(self.input.elem).after(self.canvas.elem);
+
+  self.canvas.pos = {
+    x: $(self.canvas.elem).position().left,
+    y: $(self.canvas.elem).position().top
+  };
+  
+  self.ctx = self.canvas.elem.getContext('2d');
+
+  /* Load our image */
+  imageObj = new Image(self.input.size.x, self.input.size.y);
+  imageObj.src = self.input.src;
+
+  /* Draw it to the canvas as soon as it's loaded */
+  imageObj.onload = function () {
+    self.ctx.drawImage(imageObj, 0, 0, self.input.size.x, self.input.size.y);
+  };
+
+  /* Register our event handlers */
+  $(self.canvas.elem).on('mousedown', function (e) { self.startDrawing(e); });
+  $(self.canvas.elem).on('mouseup', function (e) { self.stopDrawing(e); });
+  $(self.canvas.elem).on('mouseout', function (e) { self.stopDrawing(e); });
+  $(self).closest('form').on('submit', function () {
+    self.writeVal();
+  });
+}
+
+ScribbleInput.prototype.startDrawing = function () {
+  var self = this;
+
+  $(this.canvas.elem).on('mousemove', function (e) {
+    self.draw(e);
+  });
+}
+
+ScribbleInput.prototype.stopDrawing = function () {
+ var self = this;
+  
+  if (self.lastXY.x || self.lastXY.y) {
+    $(this.canvas.elem).off('mousemove');
+    this.lastXY.x = null;
+    this.lastXY.y = null;
+  }
+  
+  self.writeVal();
+}
+
+ScribbleInput.prototype.writeVal = function () {
+  var self = this;
+
+  $(self.input.elem).val(this.canvas.elem.toDataURL());
+}
+
+ScribbleInput.prototype.draw = function (e) {
+  var self = this,
+      click = {
+        /* Don't break in Firefox */
+        x: (e.offsetX === undefined ? e.originalEvent.layerX - self.canvas.pos.x : e.offsetX),
+        y: (e.offsetY === undefined ? e.originalEvent.layerY - self.canvas.pos.y : e.offsetY)
+      };
+
+  self.ctx.beginPath();
+
+  /* If we're in the process of drawing a line,
+   * we want to make sure we're drawing in the right place
+   *
+   * Move to the position of the last draw call
+   */
+  if (self.lastXY.x || self.lastXY.y) {
+    self.ctx.moveTo(self.lastXY.x, self.lastXY.y);
+  }
+
+  self.ctx.lineTo(click.x, click.y);
+  self.ctx.strokeStyle = 'rgb(0, 0, 0)';
+  self.ctx.lineWidth = 3;
+  self.ctx.lineCap = 'round';
+  self.ctx.stroke();
+  self.ctx.closePath();
+
+  self.lastXY.x = click.x;
+  self.lastXY.y = click.y;
+}
+
+/* Iterate over inputs with our scribble attribute */
+$(function () {
+  var scribbles = $('input[data-interact="scribble"]');
+
+  $(scribbles).each(function () {
+    var scribbleInput = new ScribbleInput(this);
+    scribbleInput.init();
+  });
+});
